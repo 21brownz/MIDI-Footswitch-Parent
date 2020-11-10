f591e7576a50757e03974be57f07316f6372c168
Zach Brown <21brownz@da.org>
2020-11-09 19:21:45 -0500

    first pass at gettempo and setcolor

--- lib/I2CButton/button.cpp
+++ lib/I2CButton/button.cpp
@@ -12,12 +12,28 @@ class Button {
             this->isTapTempo = isTapTempo;
             this->rgb = rgb;
             Wire.beginTransmission(address);
+            Wire.write("c");
             Wire.write(isTapTempo);
             Wire.write(rgb);
             Wire.endTransmission();
         }
-        void setColor(uint32_t rgb);
-        uint16_t getTempo();
+        void setColor(uint32_t rgb);{
+            Wire.beginTransmission(address);
+            Wire.write("r");
+            Wire.write(rgb);
+            Wire.endTransmission();
+        }
+        uint16_t getTempo(){
+            byte recieved[] = {00000000,00000000};
+            Wire.beginTransmission(address);
+            Wire.write("t");
+            Wire.requestFrom(address, 2);
+            int i = 0;
+            while(Wire.available()){
+                recieved[i] = Wire.read();
+            }
+            return (recieved[0] << 8) | recieved[1];
+        }
         uint32_t getColor();
         bool getSwitchState();
 };
